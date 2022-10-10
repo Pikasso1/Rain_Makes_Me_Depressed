47ff39bd732fe2583b016549d95b60b0b362ec78
par1ey <nordsoe2003@gmail.com>
2022-10-10 11:12:33 +0200

    Starten på suggestion array

--- Weatherman/src/app/suggestions/suggestions.component.ts
+++ Weatherman/src/app/suggestions/suggestions.component.ts
@@ -12,6 +12,20 @@ export class SuggestionsComponent implements OnInit {
   ngOnInit(): void {
   }
 }
+var suggestionnavn = new Array(6);
+  suggestionnavn[0] = "Løbetur";
+  suggestionnavn[1] = "Gåtur";
+  suggestionnavn[2] = "Picnic";
+  suggestionnavn[3] = "";
+
+var suggestion = new Array(6);
+  suggestion[0] = "Vejret er perfect til at løbe en tur. 2km til en hurtig tur med god fart, 3 km for en lidt længere tur i et okay tempo eller en 5km tur i et roligt tempo";
+  suggestion[1] = "Hvis en løbetur ikke er noget for dig, kan en 30 minutters gåtur sørge for at holde ens krop aktiv";
+  suggestion[2] = "Hvis du har brug for lidt frisk luft, men også noget selvskab så tag med vennerne i parken";
+  suggestion[3] = "";
+
+
+
 
 var ActivityTitle = "Problem occured loading title"
 var activityExplanation = "Problem occured loading explanation text"
